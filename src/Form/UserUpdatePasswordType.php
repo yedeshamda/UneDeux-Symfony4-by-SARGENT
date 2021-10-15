be053a3166c04b923213c31ee1d375dc1be664f8
Malek Fenina <malek.fenina@esprit.tn>
2021-10-15 12:21:47 +0100

    Edit Password

---
+++ src/Form/UserUpdatePasswordType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserUpdatePasswordType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('oldPassword', PasswordType::class, [
+                'data' => '',
+                'label' => 'Taper votre Mot de passe',
+                'mapped'=>false,
+            ])
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'The password fields must match.',
+                'options' => ['attr' => ['class' => 'password-field']],
+                'required' => true,
+                'mapped'=>false,
+                'first_options' => ['label' => 'Nouveau Mot de passe'],
+                'second_options' => ['label' => 'Confirmer votre Mot de passe'],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
