28b28f210f2e60d4c0dc62a4d5ee005c37b13dfc
slimen <mami.slimen.pro@gmail.com>
2021-11-21 05:59:06 +0100

    fix responsive mobile

--- src/Controller/Admin/HomeController.php
+++ src/Controller/Admin/HomeController.php
@@ -5,7 +5,7 @@ namespace App\Controller\Admin;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+#[Route('/admin', name: 'admin_')]
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
